f627f0a2877f004f2e9ce80000ac322719832360
AttenniGiulia <giulia.attenni@gmail.com>
2017-05-17 10:29:35 +0200

    Corretta hasChild di BadGirl

--- src/People/MeetingPoint.java
+++ src/People/MeetingPoint.java
@@ -9,8 +9,8 @@ import java.util.Queue;
 public class MeetingPoint {
     private Person G;
     private Person B;
-    private Queue queueGirls = new LinkedList();
-    private Queue queueBoys = new LinkedList();
+    private Queue<Person> queueGirls = new LinkedList<Person>();
+    private Queue<Person> queueBoys = new LinkedList<Person>();
 
     MeetingPoint(){
         G = null;
@@ -21,10 +21,32 @@ public class MeetingPoint {
         if(G != null && B != null) return true;
         else return false;
     }
-    public synchronized void entry() throws InterruptedException{
-        while (isBusy()){
-            wait();
+    public synchronized void theLine(Person person){
+        String typ = person.getClass().getSimpleName();
+        if (typ.charAt(typ.length()-1) == 'l'){
+            queueGirls.add(person);
+        }
+        else{
+            queueBoys.add(person);
         }
 
+        try {
+            entry();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+    public synchronized void entry() throws InterruptedException{
+        while(!queueGirls.isEmpty() || !queueBoys.isEmpty()){
+            while (isBusy()){
+                wait();
+            }
+            if (G == null) G = queueGirls.remove();
+            if (B == null) B = queueBoys.remove();
+            LoveStory loveStory = new LoveStory(G, B);
+            loveStory.start();
+            G = null;
+            B = null;
+        }
     }
 }
